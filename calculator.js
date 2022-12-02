e85a3f4b0405a5f1ca4d2db8f2680e4a7a06530d
CarrotHands <akekajoo@gmail.com>
2022-12-01 21:57:13 -0800

    all operators function, can continue calculating after pressing equals

--- calculator.js
+++ calculator.js
@@ -3,9 +3,9 @@ const buttonSize = numberGridSize / 3;
 let numberGrid;
 
 let currentNumber = 0;
-let operandA;
-let operandB;
-let currentOperator;
+let operandA = null;
+let operandB = null;
+let currentOperator = null;
 
 
 function add(a, b) {
@@ -24,11 +24,14 @@ function divide(a, b) {
 function operate(a, b, func) {
     return func(a, b);
 }
+// function getOperandA() { return operandA; }
+// function setOperandA(operand) { operandA = operand; }
+// function getOperandB() { return operandB; }
+// function setOperandB(operand) { operandB = operand; }
 
 function onNumberClick() {
-    const numberClicked = parseInt(this.innerHTML);
-    currentNumber = (currentNumber * 10) + numberClicked;
-    document.getElementById("display").innerText = currentNumber;
+    const numberClicked = parseInt(this.innerHTML);    
+    setCurrentNumber((currentNumber * 10) + numberClicked);
 }
 
 function buildNumberGrid() {    
@@ -42,9 +45,59 @@ function buildNumberGrid() {
     }
 }
 
+function buildOperators() {
+    const addButton = document.getElementById('add');
+    const subtractButton = document.getElementById('subtract');
+    const multiplyButton = document.getElementById('multiply');
+    const divideButton = document.getElementById('divide');
+    const equalsButton = document.getElementById('equals');
 
+    addButton.addEventListener('click', (e) => {
+        if (operandA == null && currentOperator == null) {
+            operandA = currentNumber;
+            currentOperator = add;            
+            setCurrentNumber(0);            
+        }
+    });
+    subtractButton.addEventListener('click', (e) => {
+        if (operandA == null && currentOperator == null) {
+            operandA = currentNumber;
+            currentOperator = subtract;            
+            setCurrentNumber(0);            
+        }
+    });
+    multiplyButton.addEventListener('click', (e) => {
+        if (operandA == null && currentOperator == null) {
+            operandA = currentNumber;
+            currentOperator = multiply;            
+            setCurrentNumber(0);            
+        }
+    });
+    divideButton.addEventListener('click', (e) => {
+        if (operandA == null && currentOperator == null) {
+            operandA = currentNumber;
+            currentOperator = divide;            
+            setCurrentNumber(0);            
+        }
+    });
+    equalsButton.addEventListener('click', (e) => {        
+        if (operandA != null && currentOperator != null && operandB == null) {            
+            operandB = currentNumber;
+            setCurrentNumber(currentOperator(operandA, operandB));
+            operandA = null;
+            operandB = null;
+            currentOperator = null;
+        }
+    });
+}
+function setCurrentNumber(number) {
+    currentNumber = number;
+    document.getElementById("display").innerText = currentNumber;
+}
 window.onload = function() {
     numberGrid = document.querySelector('.grid-numbers');
     numberGrid.style.gridTemplateColumns = `repeat(3, ${buttonSize}px)`;
     buildNumberGrid();
+    buildOperators();
+    setCurrentNumber(0);
 };
