efd92a183595b24817c3c953cd43d2d043fa42fe
CarrotHands <akekajoo@gmail.com>
2022-12-01 20:46:50 -0800

    calculator number buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+const numberGridSize = 512; //pixels
+const buttonSize = numberGridSize / 3;
+let numberGrid;
+
+
 function add(a, b) {
     return a + b;
 }
@@ -14,3 +19,18 @@ function divide(a, b) {
 function operate(a, b, func) {
     return func(a, b);
 }
+
+function buildNumberGrid() {    
+    for (let i = 9; i >= 0; i--) {
+        const num = document.createElement('button');
+        num.classList.add("number");
+        num.style.height = `${buttonSize}px`;
+        num.innerHTML = i;
+        numberGrid.appendChild(num);
+    }
+}
+window.onload = function() {
+    numberGrid = document.querySelector('.grid-numbers');
+    numberGrid.style.gridTemplateColumns = `repeat(3, ${buttonSize}px)`;
+    buildNumberGrid();
+};
